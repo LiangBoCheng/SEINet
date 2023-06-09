639cd49a7aba5ec8cc85bf1242b9ed3c0895a679
LiangBocheng <1982705535@qq.com>
2023-06-09 19:28:10 +0800

    Add files via upload

---
+++ test_SEINet.py
@@ -0,0 +1,51 @@
+import torch
+import torch.nn.functional as F
+
+import numpy as np
+import pdb, os, argparse
+import imageio
+import time
+
+from model.SEINet_EfficientNetB7 import SEINet
+from utils.data import test_dataset
+
+torch.cuda.set_device(0)
+parser = argparse.ArgumentParser()
+parser.add_argument('--testsize', type=int, default=256, help='testing size')  # The test size of SEINet_ResNet50 and SEINet2_ResNet50 is 352
+opt = parser.parse_args()
+
+dataset_path = './datasets/'
+
+model = SEINet()
+model.load_state_dict(torch.load('./models/SEINet_EfficientNetB7_ORSSD.pth'))
+
+model.cuda()
+model.eval()
+
+test_datasets = ['ORSSD']
+
+for dataset in test_datasets:
+    save_path = './results/EfficientNetB7/' + dataset + '/'
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    image_root = dataset_path + dataset + '/test-images/'
+    print(dataset)
+    gt_root = dataset_path + dataset + '/test-labels/'
+    test_loader = test_dataset(image_root, gt_root, opt.testsize)
+    time_sum = 0
+    for i in range(test_loader.size):
+        image, gt, name = test_loader.load_data()
+        gt = np.asarray(gt, np.float32)
+        gt /= (gt.max() + 1e-8)
+        image = image.cuda()
+        time_start = time.time()
+        res, s1_sig, e1, s2, s2_sig, e2,  s3, s3_sig, e3, s4, s4_sig, e4 = model(image)
+        time_end = time.time()
+        time_sum = time_sum+(time_end-time_start)
+        res = F.upsample(res, size=gt.shape, mode='bilinear', align_corners=False)
+        res = res.sigmoid().data.cpu().numpy().squeeze()
+        res = (res - res.min()) / (res.max() - res.min() + 1e-8)
+        imageio.imsave(save_path+name, res)
+        if i == test_loader.size-1:
+            print('Running time {:.5f}'.format(time_sum/test_loader.size))
+            print('Average speed: {:.4f} fps'.format(test_loader.size/time_sum))
